b6389c53d3f6280cf78e437e0c5272e6e2139847
zhangweican <515632400@qq.com>
2016-05-04 17:37:21 +0800

    支持jar文件读取jpdl文件

--- trunk/src/main/java/com/leweiyou/jbpm/JBPM.java
+++ trunk/src/main/java/com/leweiyou/jbpm/JBPM.java
@@ -1,6 +1,11 @@
 package com.leweiyou.jbpm;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import javax.annotation.PostConstruct;
 
@@ -46,21 +51,47 @@ public class JBPM {
 		taskService = processEngine.getTaskService();
 		identityService = processEngine.getIdentityService();
 		//发布jpdl下的所有配置文件
+		if(JBPM.class.getResource("").getPath().contains(".jar!")){
+			try {
+				JarFile jfile = new JarFile(new File(JBPM.class.getResource("").getPath().split("!")[0].replace("file:/", "")));
+				Enumeration<JarEntry> files = jfile.entries();
+				while (files.hasMoreElements()) {
+					JarEntry entry = (JarEntry) files.nextElement();
+					if (entry.getName().startsWith(Commons.PATH_JPDL) && entry.getName().endsWith(".jpdl.xml")){
+						try {
+							repositoryService.createDeployment().addResourceFromClasspath(entry.getName()).deploy();
+						} catch (RuntimeException e) {
+							if(e.getMessage().trim().endsWith("already exists")){
+								logger.info("jbdl file were deployed from cs-jbpm.jar:" );
+							}else{
+								logger.error("deploy jbdl file error from cs-jbpm.jar:", e);
+							}
+						}
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
 		String path = JBPM.class.getResource("/").getPath() + "../classes/" + Commons.PATH_JPDL;
 		File f = new File(path);
 		if(f.isDirectory()){
 			for(File file : f.listFiles()){
 				try {
 					repositoryService.createDeployment().addResourceFromFile(file).deploy();
-				} catch (Exception e) {
+				} catch (RuntimeException e) {
 					if(e.getMessage().trim().endsWith("already exists")){
-						logger.error("jbdl file were deployed:" + file.getName());
+						logger.info("jbdl file were deployed:" + file.getName());
 					}else{
 						logger.error("deploy jbdl file error:" + file.getName(), e);
 					}
 				}
 			}
 		}
+			
 	}
 
 	
